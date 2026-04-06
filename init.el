7eb4ab388e58d246a4c6e7f28dbb99012e66c1ea
FelixHuoEZ <13887263+FelixHuoEZ@users.noreply.github.com>
2026-04-06 13:17:08 +0800

    load init-rime during startup

--- init.el
+++ init.el
@@ -217,6 +217,7 @@
 ;; Allow users to provide an optional "init-local" containing personal settings
 ;;----------------------------------------------------------------------------
 (require 'init-local nil t)
+(require 'init-rime nil t)
 
 
 ;;----------------------------------------------------------------------------
